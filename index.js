f8441a4461249df0b2b4b0350242f7d697aea62f
gauravjsh8 <gauravjsh8@gmail.com>
2026-03-28 20:43:05 +0200

    database connection

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
+import "dotenv/config";
+import { connectToDb } from "./config/db.js";
 
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the library management system");
 });
 
-app.listen(PORT, () => {
-  console.log("Welcome to the library management system");
-});
+const startServer = async () => {
+  try {
+    await connectToDb();
+    app.listen(PORT, () => {
+      console.log(`Server is running on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server ", error);
+  }
+};
+
+startServer();
