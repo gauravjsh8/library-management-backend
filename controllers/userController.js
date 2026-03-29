a1aad565b69771408bde78e91510f35fd68681c4
gauravjsh8 <gauravjsh8@gmail.com>
2026-03-29 16:24:30 +0300

    added login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcryptjs";
 import { streamUpload } from "../utils/cloudinaryUpload.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -55,6 +56,60 @@ export const registerUser = async (req, res) => {
     });
   } catch (error) {
     console.log("Error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const isPasswordMatch = await bcrypt.compare(password, hashedPassword);
+
+    if (!isPasswordMatch) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        name: existingUser.name,
+        email: existingUser.email,
+        id: existingUser._id,
+        role: existingUser.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user: existingUser,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser } from "../controllers/userController.js";
 import { upload } from "../middlewares/multer.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/register", upload.single("image"), registerUser);
+userRouter.post("/login", loginUser);
